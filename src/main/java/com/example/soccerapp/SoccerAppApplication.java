6bf748bb1abc4788157eb84881165b50db37b231
jasonjayo <jasonjayo2002@gmail.com>
2024-10-22 01:32:34 +0100

    Added Spring Boot

---
+++ src/main/java/com/example/soccerapp/SoccerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.soccerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerAppApplication.class, args);
+    }
+
+}
