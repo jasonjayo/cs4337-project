17bcabb3af17588c90ab9ad374d6ec5922cebbde
jasonjayo <jasonjayo2002@gmail.com>
2024-11-08 01:57:34 +0000

    Added Eureka server

---
+++ eureka-service/src/main/java/com/example/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableFeignClients
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
