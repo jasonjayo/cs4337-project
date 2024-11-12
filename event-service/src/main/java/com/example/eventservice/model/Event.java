d3f8a6e7b2ff7ee7b970c9de60e493e8ca74846b
Kevin Collins <kevinmscollins@gmail.com>
2024-11-12 18:28:14 +0000

    feat: changes made to work in Azure

--- event-service/src/main/java/com/example/eventservice/model/Event.java
+++ event-service/src/main/java/com/example/eventservice/model/Event.java
@@ -9,20 +9,36 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "events", schema = "dbo")
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long event_id;
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(name = "event_type")
     private String eventType;
+
+    @Column(name = "event_date")
     private LocalDateTime eventDate;
+
     private String location;
+
     @Column(columnDefinition = "TEXT")
     private String description;
-    @Column(columnDefinition = "TEXT")
+
+    @Column(name = "team_sheet", columnDefinition = "TEXT")
     private String teamSheet;
+
     private String title;
+
+    @Column(name = "start_time")
     private LocalDateTime startTime;
+
+    @Column(name = "end_time")
     private LocalDateTime endTime;
+
+    @Column(name = "team_id")
     private Long teamId;
 }
